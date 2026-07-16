7cf1ec8658996b8ef4b75feb02506142ae485a83
Imesh010 <ikavinda010@gmail.com>
2026-07-16 09:08:21 +0530

    MAC addr and channel for c3

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,26 @@
 void setup()
 {
     Serial.begin(115200);
+    delay(2000);
 
     WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
 
-    Serial.println();
-    Serial.print("ESP32 STA MAC Address: ");
+    Serial.println("========== ESP32-C3 ==========");
+
+    Serial.print("MAC Address : ");
     Serial.println(WiFi.macAddress());
+
+    Serial.print("WiFi Channel: ");
+    Serial.println(WiFi.channel());
+
+    Serial.println("==============================");
 }
 
 void loop()
 {
+    delay(5000);
+
+    Serial.print("Channel: ");
+    Serial.println(WiFi.channel());
 }
