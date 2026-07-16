1aec476050539becd4c6499d4533b0a491b7c8ba
Imesh010 <ikavinda010@gmail.com>
2026-07-16 09:20:45 +0530

    mac addr and channel

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <esp_now.h>
-
-uint8_t masterMAC[] = {0x30, 0x76, 0xF5, 0xE7, 0xE6, 0x24};
-
-typedef struct
-{
-    int nodeID;
-    int counter;
-} SensorData;
-
-SensorData data;
-
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-    Serial.print("Send Status : ");
-
-    if (status == ESP_NOW_SEND_SUCCESS)
-        Serial.println("Success");
-    else
-        Serial.println("Failed");
-}
 
 void setup()
 {
     Serial.begin(115200);
-    delay(1000);
+    delay(2000);
 
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
 
-    Serial.println("ESP32-C3 Sender");
-    Serial.print("My MAC : ");
+    Serial.println("========== ESP32-C3 ==========");
+
+    Serial.print("MAC Address : ");
     Serial.println(WiFi.macAddress());
 
-    if (esp_now_init() != ESP_OK)
-    {
-        Serial.println("ESP-NOW Init Failed");
-        return;
-    }
+    Serial.print("WiFi Channel: ");
+    Serial.println(WiFi.channel());
 
-    esp_now_register_send_cb(OnDataSent);
-
-    esp_now_peer_info_t peerInfo = {};
-
-    memcpy(peerInfo.peer_addr, masterMAC, 6);
-    peerInfo.channel = 1;
-    peerInfo.encrypt = false;
-
-    if (esp_now_add_peer(&peerInfo) != ESP_OK)
-    {
-        Serial.println("Failed to Add Peer");
-        return;
-    }
-
-    data.nodeID = 1;
-    data.counter = 0;
-
-    Serial.println("Sender Ready");
+    Serial.println("==============================");
 }
 
 void loop()
 {
-    data.counter++;
+    delay(5000);
 
-    esp_err_t result = esp_now_send(
-        masterMAC,
-        (uint8_t *)&data,
-        sizeof(data));
-
-    Serial.print("Counter : ");
-    Serial.println(data.counter);
-
-    Serial.print("Send Result : ");
-    Serial.println(result);
-
-    delay(2000);
+    Serial.print("Channel: ");
+    Serial.println(WiFi.channel());
 }
